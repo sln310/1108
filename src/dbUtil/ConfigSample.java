9724b90532683da5432b8e1619e681f456b11dd0
sh310310 <110559375+sh310310@users.noreply.github.com>
2022-11-09 02:49:23 -0800

    commit

---
+++ src/dbUtil/ConfigSample.java
@@ -0,0 +1,15 @@
+package dbUtil;
+/**
+ * 
+ * Rename this file to Config.java
+ * 
+ * Add your supabase or any JDBC credentials
+ */
+
+public class ConfigSample {
+
+    protected static final String URL = "jdbc:postgresql://db.wdqjmjaqaozvxpltyjiu.supabase.co:5432/postgres";
+    protected static final String USERNAME = "postgres";
+    protected static final String PASSWORD = "mPuNQVXwDGW3V4oW";
+    
+}
---
+++ src/home/HomeController.java
@@ -0,0 +1,90 @@
+package home;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class HomeController implements Initializable{
+    
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField department;
+    
+    @FXML
+    private TableView<EmployeeData> employeeDataTableView;
+    @FXML
+    private TableColumn<EmployeeData, String> idColumn;
+    @FXML
+    private TableColumn<EmployeeData, String> nameColumn;
+    @FXML
+    private TableColumn<EmployeeData, String> departmentColumn;
+
+    @FXML
+    private Button addEntryBtn;
+    @FXML
+    private Button clearBtn;
+
+    //instantiate a model
+    HomeModel homeModel = null;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.homeModel = new HomeModel();
+        this.loadEmployeeData();        
+    }
+
+    //load data
+    @FXML
+    public void loadEmployeeData(){
+
+        this.idColumn.setCellValueFactory( new PropertyValueFactory<EmployeeData, String>("id"));
+        this.nameColumn.setCellValueFactory( new PropertyValueFactory<EmployeeData, String>("name"));
+        this.departmentColumn.setCellValueFactory( new PropertyValueFactory<EmployeeData, String>("department"));
+
+        this.employeeDataTableView.setItems(homeModel.getEmployees());
+    }
+
+    //add employee
+    @FXML
+    private void addEmployee(ActionEvent event){
+        homeModel.addEmployee(this.name.getText(), this.department.getText());
+        this.loadEmployeeData();
+        this.clearFields(null);
+    }
+
+    //update employee
+    @FXML
+    private void updateEmployee(ActionEvent event){
+        EmployeeData employee = employeeDataTableView.getSelectionModel().getSelectedItem();;
+        homeModel.updateEmployee(this.name.getText() != "" ? this.name.getText() : employee.nameProperty().get() , this.department.getText() != "" ? this.department.getText() : employee.departmentProperty().get(), employee.idProperty().getValue());
+        this.loadEmployeeData();
+        this.clearFields(null);
+    }
+    
+    //delete employee
+    @FXML
+    private void deleteEmployee(ActionEvent event){
+        EmployeeData employee = employeeDataTableView.getSelectionModel().getSelectedItem();;
+        homeModel.deleteEmployee(employee.idProperty().getValue());
+        this.loadEmployeeData();
+        this.clearFields(null);
+    }
+
+
+    //clear fields
+    @FXML
+    private void clearFields(ActionEvent event){
+        this.name.setText("");
+        this.department.setText("");
+    }
+
+}
